fa697fd16b6b5bf9a0e56a2251ccd45939b19708
Yasin Ali Marouga <yasinam@stud.ntnu.no>
2023-02-01 15:04:11 +0100

    Implemented getPassages() method in Story and fixed addPassage

--- src/main/java/edu/ntnu/idatt2001/groups/group32/Story.java
+++ src/main/java/edu/ntnu/idatt2001/groups/group32/Story.java
@@ -1,6 +1,8 @@
 package edu.ntnu.idatt2001.groups.group32;
 
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * A story is a collection of passages, linked together by their links.
@@ -13,7 +15,7 @@ public class Story {
 
     private Passage openingPassage;
 
-    Story(title, openingPassage) {
+    Story(String title, Passage openingPassage) {
         this.title = title;
         this.openingPassage = openingPassage;
 
@@ -29,9 +31,14 @@ public class Story {
     }
 
     public void addPassage(Passage passage) {
-        passages.add(passage.getTitle(), passage);
+        String text = "";
+        passages.put(new Link(passage.getTitle(), passage.getTitle()), passage);
     }
     public Passage getPassage(Link link) {
         return passages.get(link);
+    }
+
+    public Collection<Passage> getPassages() {
+        return passages.values();
     }
 }
