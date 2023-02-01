c7bf38e664d31456dd2828e9316df4a5b353f1e5
Yasin Ali Marouga <yasinam@stud.ntnu.no>
2023-02-01 15:59:26 +0100

    Implemented the class Player, and added javaDoc for Story

---
+++ src/main/java/edu/ntnu/idatt2001/groups/group32/Player.java
@@ -0,0 +1,88 @@
+package edu.ntnu.idatt2001.groups.group32;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This class represents a Player.
+ * A player and its attributes can be affected by a story.
+ */
+public class Player {
+    // Name of the player.
+    private final String name;
+    // Health of a player.
+    private int health;
+    //Score of a player.
+    private int score;
+    // Gold of a player.
+    private int gold;
+    private List<String> inventory;
+
+    public Player(String name, int health, int score, int gold) {
+        this.name = name;
+        this.health = health;
+        if (score > 0) {
+            this.score = score;
+        } else {
+            throw new IllegalArgumentException("Health cannot dip below zero!");
+        }
+        this.gold = gold;
+        inventory = new ArrayList<String>();
+    }
+
+    /**
+     * This method returns the name a player.
+     * @return Name of player as String
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * THis method adds new heath points to a player.
+     * @param newHealthPoints New health points, as int.
+     */
+    public void addHealth(int newHealthPoints) {
+        this.health += newHealthPoints;
+    }
+
+    /**
+     * This method adds new score points to a player:
+     * @param newScorePoints New score points, as int.
+     */
+    public void addScore(int newScorePoints) {
+        this.score += newScorePoints;
+    }
+
+    /**
+     * This method returns the score of a player.
+     * @return Score of a player, as int.
+     */
+    public int getScore() {
+        return score;
+    }
+
+    /**
+     * This method adds new gold points to the player.
+     * @param newGoldPoints New gold points, as int.
+     */
+    public void addGold(int newGoldPoints) {
+        this.gold += newGoldPoints;
+    }
+
+    /**
+     * This method adds an item to a player's inventory.
+     * @param item Item to be added, as String.
+     */
+    public void addToInventory(String item) {
+       inventory.add(item);
+    }
+
+    /**
+     * This method returns the inventory of a player.
+     * @return Returns inventory as, List<String>
+     */
+    public List<String> getInventory() {
+        return inventory;
+    }
+}
--- src/main/java/edu/ntnu/idatt2001/groups/group32/Story.java
+++ src/main/java/edu/ntnu/idatt2001/groups/group32/Story.java
@@ -5,16 +5,22 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * This class represents a story.
  * A story is a collection of passages, linked together by their links.
  */
 public class Story {
-
+    // Title of the story.
     private String title;
-
+    // Map of all the passages in an instance of story.
     private Map<Link, Passage> passages;
-
+    // The opening passage of the story.
     private Passage openingPassage;
 
+    /**
+     * This constructor facilitates the creation of instances of the class Story.
+     * @param title
+     * @param openingPassage
+     */
     Story(String title, Passage openingPassage) {
         this.title = title;
         this.openingPassage = openingPassage;
@@ -22,22 +28,43 @@ public class Story {
         passages = new HashMap<Link, Passage>();
     }
 
+    /**
+     * This method returns the title a story.
+     * @return Title of story as String.
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * This method returns the opening passage of a story.
+     * @return openingPassage, as int.
+     */
     public Passage getOpeningPassage() {
         return openingPassage;
     }
 
+    /**
+     * This method adds a passage to the passages Map.
+     * @param passage Passage to be added, as Passage
+     */
     public void addPassage(Passage passage) {
-        String text = "";
         passages.put(new Link(passage.getTitle(), passage.getTitle()), passage);
     }
+
+    /**
+     * This method returns a passage from the passages Map, using the passage's Link.
+     * @param link Link of the passage, as Link.
+     * @return Passage as passage.
+     */
     public Passage getPassage(Link link) {
         return passages.get(link);
     }
 
+    /**
+     * This method return the Map passages.
+     * @return Passages, as Collection<Passage>.
+     */
     public Collection<Passage> getPassages() {
         return passages.values();
     }
