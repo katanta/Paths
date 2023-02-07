92b06830182880029323bd973720e4e6262f5ba3
Yasin Ali Marouga <yasinam@stud.ntnu.no>
2023-02-07 11:40:54 +0100

    Recatored 'Goals' to 'Goal'.

--- src/main/java/edu/ntnu/mappe32/Goal.java
+++ src/main/java/edu/ntnu/mappe32/Goal.java
@@ -4,7 +4,7 @@ package edu.ntnu.mappe32;
  * Goals are used to check the values of the attributes of a player.
  * Players can either fulfill or not fulfill a goal.
  */
-public interface Goals {
+public interface Goal {
     /**
      * This method decides which player a goal-check should be executed on.
      * @param player The player a goal should be executed on, as Player.
