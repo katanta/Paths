401665767f3f6f69328afc88239d801cb454ab8b
Kris.JM <krisjm@stud.ntnu.no>
2023-02-07 14:44:00 +0100

    created first test for isFulfilled method in InventoryGoal, and some random test-data to copy from

--- src/test/java/edu/ntnu/mappe32/InventoryGoalTest.java
+++ src/test/java/edu/ntnu/mappe32/InventoryGoalTest.java
@@ -11,12 +11,13 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class InventoryGoalTest {
+    Player player = null;
     @BeforeEach
     void setUp() {
-        Player player = new Player("Olav", 100, 0, 25);
+        player = new Player("Olav", 100, 0, 25);
         player.addToInventory("Master Sword");
         player.addToInventory("Length of Rope");
         player.addToInventory("Legendary Ring of Fortifying");
@@ -29,21 +30,10 @@ public class InventoryGoalTest {
     }
 
     @Test
-    void isFulfilledReturnsTrueWhenInventoryContainsMandatoryItemsToFulfillGoal() {
-        Player player = new Player("Olav", 100, 0, 25);
-        player.addToInventory("Master Sword");
-        player.addToInventory("Length of Rope");
-        player.addToInventory("Legendary Ring of Fortifying");
-        player.addToInventory("Potato");
-        player.addToInventory("The King's Crown");
-        player.addToInventory("Potion of Healing");
-        Link l = new Link("a", "b");
-        Passage p = new Passage("a", "b");
-        Story s = new Story("test", p);
+    void isFulfilledReturnsTrueWhenInventoryContainsSingularMandatoryItemToFulfillGoal() {
         ArrayList<String> mandatoryItems = new ArrayList<String>();
         mandatoryItems.add("The King's Crown");
         InventoryGoal kingsCrown = new InventoryGoal(mandatoryItems);
         assertTrue(kingsCrown.isFulfilled(player));
     }
-    //The first file for tests...
 }
