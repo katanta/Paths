853f0a258b16542204989041f349747bd79829e6
Kris.JM <krisjm@stud.ntnu.no>
2023-02-07 13:42:47 +0100

    Implemented GoldGoal class, minus a body on the execute method

---
+++ src/main/java/edu/ntnu/mappe32/GoldGoal.java
@@ -0,0 +1,32 @@
+package edu.ntnu.mappe32;
+
+/**
+ * Goal for minimum gold.
+ * @author Kristians J.Matrevics
+ * @version 0.1
+ */
+public class GoldGoal implements Goal{
+    private final int minimumGold;
+
+    /**
+     * This constructor facilitates the creation of instances of the class GoldGoal.
+     * @param minimumGold Gold required for a goal to be reached, as int.
+     * @since 0.1
+     */
+    public GoldGoal(int minimumGold) {
+        this.minimumGold = minimumGold;
+    }
+
+    @Override
+    public void execute(Player player) {
+        //TODO: Implement this
+    }
+    /**
+     * This method checks if the gold goal is reached.
+     * @param player The player a goal-check should be executed on, as Player.
+     * @return Whether the player has fulfilled this goal, as boolean.
+     */
+    public boolean isFulfilled(Player player) {
+        return player.getGold() >= minimumGold;
+    }
+}
