a4b926493c0b220aed7aa69a5e91c46deed4b50c
Yasin Ali Marouga <yasinam@stud.ntnu.no>
2023-03-03 16:59:46 +0100

    JavaDoc the mandatoryItems field and refactor the return threshold of isFulfilled

--- src/main/java/edu/ntnu/mappe32/goal_related/InventoryGoal.java
+++ src/main/java/edu/ntnu/mappe32/goal_related/InventoryGoal.java
@@ -5,9 +5,8 @@ import edu.ntnu.mappe32.Player;
 import java.util.List;
 
 public class InventoryGoal implements Goal {
-    private List<String> mandatoryItems;
-
-
+    // List of mandatory items that a player needs to achieve an InventoryGoal
+    private final List<String> mandatoryItems;
     /**
      * This constructor facilitates the creation of instances of the class InventoryGoal.
      * @param mandatoryItems items required to reach a particular goal, as a list of strings.
@@ -20,14 +19,14 @@ public class InventoryGoal implements Goal {
     /**
      * This method checks a player's inventory to see if the item goal is met.
      * @param player The player being inspected to see if the requirements/goals are met.
-     * @return whether player's inventory contains all the required items.
+     * @return Whether  player's inventory contains all the required items.
      * @since 0.1
      */
     @Override
     public boolean isFulfilled(Player player) {
         int mandatoryItemCount = (int) player.getInventory().stream().
-                filter(item -> mandatoryItems.contains(item)).count();
-        return mandatoryItemCount >= mandatoryItems.size();
+                filter(mandatoryItems::contains).count();
+        return mandatoryItemCount == mandatoryItems.size();
     }
 }
 
