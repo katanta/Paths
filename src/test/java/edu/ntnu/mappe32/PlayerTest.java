d632f8a31373cec918fa86959167aad52ddf54d7
Yasin Ali Marouga <yasinam@stud.ntnu.no>
2023-03-04 08:27:36 +0100

    Implement nested tests for addGold(), getInventory() and addScore() in PlayerTest

--- src/test/java/edu/ntnu/mappe32/PlayerTest.java
+++ src/test/java/edu/ntnu/mappe32/PlayerTest.java
@@ -1,13 +1,15 @@
 package edu.ntnu.mappe32;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PlayerTest {
     Player mutumbu;
@@ -52,12 +54,14 @@ public class PlayerTest {
         this.pumbasTestInventory = new ArrayList<String>(Arrays.asList("N'Tofos", "Orange",
                 "Apple", "Machete", "Knife", "Paperclip", "Rolex", "Key"));
     }
+    @DisplayName("getName() returns name")
     @Test
     void getNameReturnsName() {
         assertEquals("Mutumbu", mutumbu.getName());
         assertEquals("Mufasa", mufasa.getName());
         assertEquals("Pumba", pumba.getName());
     }
+    @DisplayName("getScore() returns score")
     @Test
     void getScoreReturnsScore() {
         assertEquals(0, mutumbu.getScore());
@@ -65,13 +69,14 @@ public class PlayerTest {
         assertEquals(40, pumba.getScore());
         
     }
+    @DisplayName("getHealth() returns health")
     @Test
     void getHealthReturnsHealth() {
         assertEquals(150, mutumbu.getHealth());
         assertEquals(200, mufasa.getHealth());
         assertEquals(202, pumba.getHealth());
     }
-
+    @DisplayName("getGold() returns gold")
     @Test
     void getGoldReturnsGold() {
         assertEquals(25, mutumbu.getGold());
@@ -79,34 +84,72 @@ public class PlayerTest {
         assertEquals(100, pumba.getGold());
     }
 
-    @Test
-    void getInventoryReturnsInventoryInOriginalOrder() {
-        assertEquals(mutumbusTestInventory, mutumbu.getInventory());
-        assertEquals(mufasasTestInventory, mufasa.getInventory());
-        assertEquals(pumbasTestInventory, pumba.getInventory());
+    @DisplayName("addHealth()")
+    @Nested
+    public class AddHealthTest {
+        @DisplayName("adds health")
+        @Test
+        void addHealthAddsHealth() {
+            mutumbu.addHealth(50);
+            assertEquals(200, mutumbu.getHealth());
+        }
+        @DisplayName("does not reduce health below zero")
+        @Test
+        void addHealthDoesNotReduceHealthBelowZero() {
+            mufasa.addHealth(-2 * mufasa.getHealth());
+            assertEquals(0, mufasa.getHealth());
+        }
     }
-    @Test
-    void addHealthAddsPositiveHealth() {
-        mutumbu.addHealth(50);
-        assertEquals(200, mutumbu.getHealth());
-    }
-    @Test
-    void addGoldAddsGold() {
-        mutumbu.addGold(100);
-        mufasa.addGold(-100);
-        pumba.addGold(-150);
-        assertEquals(125, mutumbu.getGold());
-        assertEquals(0, mufasa.getGold());
-        assertEquals(-50,pumba.getGold());
-    }
-    @Test
-    void addHealthDoesNotSetOrReduceHealthBelowZero() {
-        mufasa.addHealth(-2 * mufasa.getHealth());
-        assertEquals(0, mufasa.getHealth());
+    @DisplayName("addGold()")
+    @Nested
+    class AddGoldTest {
+        @DisplayName("adds gold")
+        @Test
+        void addGoldAddsGold() {
+            mutumbu.addGold(100);
+            mufasa.addGold(-100);
+            pumba.addGold(-100);
+            assertEquals(125, mutumbu.getGold());
+            assertEquals(0, mufasa.getGold());
+            assertEquals(0,pumba.getGold());
+        }
+        @DisplayName("throws IllegalArgumentException if player does not have enough gold")
+        @Test
+        void addGoldThrowsIllegalArgumentExceptionIfPlayerDoesNotHaveEnoughGold() {
+            assertThrows(IllegalArgumentException.class, () -> mutumbu.addGold(-10000));
+            assertThrows(IllegalArgumentException.class, () -> mufasa.addGold(-100000000));
+            assertThrows(IllegalArgumentException.class, () -> pumba.addGold(-10000));
+        }
     }
 
+    @DisplayName("getInventory()")
+    @Nested
+    class getInventoryTests {
+        @DisplayName("returns inventory")
+        @Test
+        void getInventoryReturnsInventory() {
+            assertEquals(mutumbusTestInventory, mutumbu.getInventory());
+            assertEquals(mufasasTestInventory, mufasa.getInventory());
+            assertEquals(pumbasTestInventory, pumba.getInventory());
+        }
+        @DisplayName("returns copy of inventory")
+        @Test
+        void getInventoryReturnsCopyOfInventory() {
+            assertEquals(mutumbusTestInventory, mutumbu.getInventory());
+            assertEquals(mufasasTestInventory, mufasa.getInventory());
+            assertEquals(pumbasTestInventory, pumba.getInventory());
+            mutumbusTestInventory.add("A");
+            mufasasTestInventory.add("B");
+            pumbasTestInventory.add("C");
+            assertNotEquals(mutumbusTestInventory, mutumbu.getInventory());
+            assertNotEquals(mufasasTestInventory, mufasa.getInventory());
+            assertNotEquals(pumbasTestInventory, pumba.getInventory());
+        }
+
+    }
+    @DisplayName("addToInventory() adds a String to inventory")
     @Test
-    void addInventoryAddsItemToInventory() {
+    void addToInventoryAddsItemToInventory() {
         mutumbu.addToInventory("Glue");
         mufasa.addToInventory("Soup");
         pumba.addToInventory("Spaghetti");
@@ -117,14 +160,28 @@ public class PlayerTest {
         assertEquals(mufasasTestInventory, mufasa.getInventory());
         assertEquals(pumbasTestInventory, pumba.getInventory());
     }
-
-    @Test
-    void addScoreAddsScore() {
-        mutumbu.addScore(20);
-        mufasa.addScore(-120);
-        pumba.addScore(-30);
-        assertEquals(20, mutumbu.getScore());
-        assertEquals(-70, mufasa.getScore());
-        assertEquals(10, pumba.getScore());
+    @DisplayName("addScore()")
+    @Nested
+    public class AddScoreTest {
+        @DisplayName("adds score")
+        @Test
+        void addScoreAddsScore() {
+            mutumbu.addScore(20);
+            mufasa.addScore(100);
+            pumba.addScore(-30);
+            assertEquals(20, mutumbu.getScore());
+            assertEquals(150, mufasa.getScore());
+            assertEquals(10, pumba.getScore());
+        }
+        @DisplayName("does not reduce score below zero")
+        @Test
+        void addScoreDoesNotReduceScoreBelowZero() {
+            mutumbu.addScore(-51);
+            mufasa.addScore(-100);
+            pumba.addScore(-1000);
+            assertEquals(0, mutumbu.getScore());
+            assertEquals(0, mufasa.getScore());
+            assertEquals(0, pumba.getScore());
+        }
     }
 }
