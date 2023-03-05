3327ec9118ba0c07b9dde05741fa1cc0bd729755
Yasin Ali Marouga <yasinam@stud.ntnu.no>
2023-03-05 20:35:08 +0100

    Fix removePassage() of Story
    In order to remove a passage we must create a new key whose title and name are equal to the removal link's reference, as this will be equal to the desired passage's key

--- src/main/java/edu/ntnu/mappe32/story_related/Story.java
+++ src/main/java/edu/ntnu/mappe32/story_related/Story.java
@@ -5,7 +5,6 @@ import edu.ntnu.mappe32.story_related.Link;
 import edu.ntnu.mappe32.story_related.Passage;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 /**
  * This class represents a story.
@@ -19,7 +18,7 @@ public class Story {
     /**
      * Map of all the passages in an instance of story
      */
-    private Map<Link, Passage> passages;
+    private final Map<Link, Passage> passages;
     /**
      * The opening passage of the story
      */
@@ -32,7 +31,7 @@ public class Story {
      * @param title Title of a story, as String
      * @param openingPassage Opening passage of a story, as Passage
      */
-    public Story(final String title, final Passage openingPassage) throws IllegalStateException {
+    public Story(final String title, final Passage openingPassage) throws IllegalArgumentException {
         if (title.isBlank()) {
             throw new IllegalArgumentException("A story must have a title");
         }
@@ -43,7 +42,6 @@ public class Story {
         this.openingPassage = openingPassage;
         passages = new HashMap<Link, Passage>();
     }
-
     /**
      * This method returns the title a story.
      * @return Title of story as String.
@@ -83,25 +81,22 @@ public class Story {
     public Collection<Passage> getPassages() {
         return passages.values();
     }
-
     /**
      * This method removes a passage from Map <Link, Passage>.
      * Throws IllegalArgumentException if the link refers to multiple or zero passages.
      * @param removalLink A link of the passage to be removed, as Link
      */
     public void removePassage(Link removalLink) {
-        int matches = (int) passages.keySet().stream().map(Link::getReference)
-                .filter(references -> references.contains(removalLink.getReference())).count();
+        int matches = (int) getPassages().stream()
+                .flatMap(passages -> passages.getLinks().stream()).map(Link::getReference)
+                .filter(references -> references.equals(removalLink.getReference())).count();
         if (matches > 1) {
-            throw new IllegalArgumentException("The passage has multiple links which refer to it and cannot be removed");
+            throw new IllegalStateException("The passage has multiple links which refer to it and cannot be removed");
         }
-        if (matches == 0) {
-            throw new IllegalArgumentException("The link refers to no passage");
-        }
-        passages.remove(removalLink);
+        passages.remove(new Link(removalLink.getReference(), removalLink.getReference()));
     }
-
-    /** This method gets the broken links of a story.
+    /**
+     * This method gets the broken links of a story.
      * A broken link is defined as a link that belongs to a passage that does not yet refer to any passage,
      * meaning that attempting to use it in an actual story would not return an existing passage.
      * @return Broken links in a story, as List<Link>
