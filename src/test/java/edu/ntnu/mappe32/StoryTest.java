a93c6ef4caff57c7eb3b08690246c0c14dd93e3a
Yasin Ali Marouga <yasinam@stud.ntnu.no>
2023-03-05 21:12:27 +0100

    Implement nested tests for removePassage() and contructor, and a test method for getBrokenLinks()

--- src/test/java/edu/ntnu/mappe32/StoryTest.java
+++ src/test/java/edu/ntnu/mappe32/StoryTest.java
@@ -5,6 +5,8 @@ import edu.ntnu.mappe32.story_related.Link;
 import edu.ntnu.mappe32.story_related.Passage;
 import edu.ntnu.mappe32.story_related.Story;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -12,28 +14,26 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class StoryTest {
-
+class StoryTest {
     Link climbTree;
     Link kickTree;
     Link shakeTree;
     Link runHome;
     Link scratchHead;
-    Link runToKilimanjaro;
     Link franticallyWaveHands;
     Link takeABite;
     Passage openingPassage;
     Passage kickedShakedTree;
     Passage climbedTree;
+    Passage franticallyWavedHands;
     Story storyOfAfrica;
     Action remove10HealthPoints;
     Action add1ScorePoint;
     Action add10GoldPoints;
     Action addMacheteToInventory;
-    List<Action> assertionActions;
-    List<Link> assertionLinks;
+    List<Link> assertionBrokenLinks;
     Map<Link, Passage> assertionPassages;
 
     @BeforeEach
@@ -42,13 +42,6 @@ public class StoryTest {
         add1ScorePoint = new ScoreAction(1);
         add10GoldPoints = new GoldAction(10);
         addMacheteToInventory = new InventoryAction("Machete");
-
-        assertionActions = new ArrayList<>();
-
-        assertionActions.add(remove10HealthPoints);
-        assertionActions.add(add1ScorePoint);
-        assertionActions.add(add10GoldPoints);
-        assertionActions.add(addMacheteToInventory);
 
         openingPassage = new Passage("Opening Passage", "You just found the enchanted apple tree " +
                 "you've been searching for your whole adventure around Serengeti. " +
@@ -68,7 +61,7 @@ public class StoryTest {
         franticallyWaveHands = new Link("Frantically wave hands", "Frantically waved Hands Passage");
         takeABite = new Link("Take a bite", "Took a Bite");
 
-
+        franticallyWavedHands = new Passage("Frantically waved Hands Passage", "Your hands fell off");
         kickedShakedTree = new Passage("Kicked/shaked tree Passage", "The golden apple fell on your head. " +
                 "Your head is bumped. " + "A killer bee flies by!");
 
@@ -76,18 +69,15 @@ public class StoryTest {
 
         scratchHead = new Link("Scratch head. You screathed your head. It did not bring any relief. You took 10 damage.",
                 "Scratched head 10 damage Passage");
-        runToKilimanjaro = new Link("Run to Kilimanjaro", "Run to Kilimanjaro Passage");
 
-        assertionLinks = new ArrayList<>();
+        assertionBrokenLinks = new ArrayList<>();
 
         openingPassage.addLink(climbTree);
 
-        assertionLinks.add(climbTree);
-        assertionLinks.add(shakeTree);
-        assertionLinks.add(franticallyWaveHands);
-        assertionLinks.add(takeABite);
-        assertionLinks.add(runHome);
-        assertionLinks.add(scratchHead);
+        assertionBrokenLinks.add(takeABite);
+        assertionBrokenLinks.add(runHome);
+        assertionBrokenLinks.add(scratchHead);
+        assertionBrokenLinks.add(franticallyWaveHands);
 
         climbedTree.addLink(climbTree);
         climbedTree.addLink(shakeTree);
@@ -103,31 +93,83 @@ public class StoryTest {
 
         assertionPassages.put(new Link(kickedShakedTree.getTitle(), kickedShakedTree.getTitle()), kickedShakedTree);
     }
-
+    @DisplayName("constructor")
+    @Nested
+    class ConstructorTest {
+        @DisplayName("throws IllegalArgumentException when title is blank")
+        @Test
+        void constructorThrowsIllegalArgumentExceptionWhenTitleIsBlank() {
+            assertThrows(IllegalArgumentException.class, () -> new Story(" ", openingPassage));
+        }
+        @DisplayName("throws IllegalArgumentException when opening passage is null")
+        @Test
+        void constructorThrowsIllegalArgumentExceptionWhenOpeningPassageIsNull() {
+            assertThrows(IllegalArgumentException.class, () -> new Story("Story of Africa", null));
+        }
+    }
+    @DisplayName("getTitle() returns title")
     @Test
     void getTitleReturnsTitle() {
         assertEquals("Story of Africa", storyOfAfrica.getTitle());
     }
-
+    @DisplayName("getOpeningPassage() returns opening passage")
     @Test
     void getOpeningPassageReturnsOpeningPassage() {
         assertEquals(openingPassage, storyOfAfrica.getOpeningPassage());
     }
-
+    @DisplayName("addPassage() adds passage")
     @Test
     void addPassageAddsPassage() {
         storyOfAfrica.addPassage(climbedTree);
         assertionPassages.put(new Link(climbedTree.getTitle(),climbedTree.getTitle()), climbedTree);
         assertEquals(assertionPassages.values().toString(), storyOfAfrica.getPassages().toString());
     }
-
+    @DisplayName("getPassage() returns passage")
     @Test
     void getPassageReturnsPassage() {
         assertEquals(kickedShakedTree, storyOfAfrica.getPassage(kickTree));
     }
-
+    @DisplayName("getPassages() returns List<Passage> passages")
     @Test
     void getPassagesReturnsPassages() {
         assertEquals(assertionPassages.values().toString(), storyOfAfrica.getPassages().toString());
+    }
+
+    @DisplayName("removePassage()")
+    @Nested
+    class RemovePassageTest {
+        @DisplayName("removes passage with 1 link referring to it")
+        @Test
+        void removesPassageWith1LinkReferringToIt() {
+            storyOfAfrica.addPassage(franticallyWavedHands);
+            storyOfAfrica.addPassage(climbedTree);
+            climbedTree.addLink(kickTree);
+            storyOfAfrica.removePassage(franticallyWaveHands);
+            assertFalse(storyOfAfrica.getPassages().contains(franticallyWavedHands));
+        }
+        @DisplayName("throws IllegalStateException when multiple links refer to a passage")
+        @Test
+        void throwsIllegalStateExceptionWhenMultipleLinksReferToAPassage() {
+            storyOfAfrica.addPassage(kickedShakedTree);
+            storyOfAfrica.addPassage(climbedTree);
+            climbedTree.addLink(kickTree);
+            climbedTree.addLink(shakeTree);
+            assertThrows(IllegalStateException.class, () -> storyOfAfrica.removePassage(kickTree));
+        }
+    }
+    @DisplayName("getBrokenLinks()")
+    @Nested
+    class GetBrokenLinksTest {
+        @DisplayName("returns all of the broken links in no particular order")
+        @Test
+        void getBrokenLinksReturnsBrokenLinks() {
+            storyOfAfrica.addPassage(kickedShakedTree);
+            storyOfAfrica.addPassage(climbedTree);
+            climbedTree.addLink(kickTree);
+            climbedTree.addLink(shakeTree);
+            assertEquals(assertionBrokenLinks.size(), storyOfAfrica.getBrokenLinks().size());
+            assertTrue(storyOfAfrica.getBrokenLinks().containsAll(assertionBrokenLinks));
+            assertTrue(assertionBrokenLinks.containsAll(storyOfAfrica.getBrokenLinks()));
+        }
     }
 }
