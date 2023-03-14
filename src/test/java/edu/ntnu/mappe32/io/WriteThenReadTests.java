4bcbaa6ae1677d23a68c1092d5d0758a56a94fab
Kris.JM <krisjm@stud.ntnu.no>
2023-03-14 15:03:32 +0100

    Create test package for reading and writing

---
+++ src/test/java/edu/ntnu/mappe32/io/WriteThenReadTests.java
@@ -0,0 +1,17 @@
+package edu.ntnu.mappe32.io;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WriteThenReadTests {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void writeStory() {
+    }
+}
