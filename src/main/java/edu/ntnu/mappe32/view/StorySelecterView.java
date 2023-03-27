bf14a84ebbba7d6dc0862ead7441863715acd5ce
Yasin Ali Marouga <yasinam@stud.ntnu.no>
2023-03-27 23:38:25 +0200

    Set the width of the columns of storyTable

--- src/main/java/edu/ntnu/mappe32/view/StorySelecterView.java
+++ src/main/java/edu/ntnu/mappe32/view/StorySelecterView.java
@@ -3,6 +3,7 @@ package edu.ntnu.mappe32.view;
 import edu.ntnu.mappe32.model.PathsFile;
 import edu.ntnu.mappe32.model.StorySelecter;
 import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
@@ -20,7 +21,8 @@ public class StorySelecterView {
 
     public StorySelecterView() {
         this.storyTable = new TableView<>();
-        this.addStoryButton = new Button("Add story");
+        this.addStoryButton = new Button("Add another .paths file");
+        addStoryButton.setLayoutX(1280);
         StackPane root = new StackPane();
         root.getChildren().addAll(storyTable, addStoryButton);
         this.scene = new Scene(root, 1280,720);
@@ -29,12 +31,14 @@ public class StorySelecterView {
     public void createStoryTable() {
         TableColumn<PathsFile, String> storyTitleColumn = new TableColumn<>("Story Title");
         storyTitleColumn.setCellValueFactory(new PropertyValueFactory<>("storyTitle"));
+        storyTitleColumn.prefWidthProperty().bind(storyTable.widthProperty().divide(9));
         TableColumn<PathsFile, String> fileLocationColumn = new TableColumn<>("File Location");
         fileLocationColumn.setCellValueFactory(new PropertyValueFactory<>("filePath"));
+        fileLocationColumn.prefWidthProperty().bind(storyTable.widthProperty().divide(1.29));
         TableColumn<PathsFile, Integer> brokenLinksColumn = new TableColumn<>("Broken Links");
         brokenLinksColumn.setCellValueFactory(new PropertyValueFactory<>("brokenLinks"));
+        brokenLinksColumn.prefWidthProperty().bind(storyTable.widthProperty().divide(9));
         storyTable.getColumns().addAll(storyTitleColumn, fileLocationColumn, brokenLinksColumn);
-        storyTable.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
     }
 
     public void fillStoryTable() {
