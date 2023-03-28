3af1edecdb3135cc4fb4837d688c8850417ac2e6
Kris.JM <krisjm@stud.ntnu.no>
2023-03-28 13:17:31 +0200

    Make configurePassageContent show surface level passage information
    Currently shows Story title, passage title and content, where content is scrollable if it is too large

--- src/main/java/edu/ntnu/mappe32/view/PassageView.java
+++ src/main/java/edu/ntnu/mappe32/view/PassageView.java
@@ -1,8 +1,15 @@
 package edu.ntnu.mappe32.view;
 
 import edu.ntnu.mappe32.controller.Game;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
 public class PassageView {
@@ -10,12 +17,36 @@ public class PassageView {
     Text passageContent;
     Label storyTitle;
     Text passageTitle;
-    public PassageView(Game game) {
+    Game game;
 
-        this.scene = scene;
+    public PassageView(Game game) {
+        this.game = game;
     }
 
     public void configurePassageContent() {
+        StackPane root = new StackPane();
+        scene = new Scene(root, 1280, 720);
+        storyTitle = new Label(game.getStory().getTitle());
+        storyTitle.setLabelFor(passageTitle);
+        storyTitle.setMinSize(10, 10);
+        storyTitle.setFont(new Font(45));
+        passageTitle = new Text(game.getStory().getOpeningPassage().getTitle());
+        passageTitle.setFont(new Font(25));
+        ScrollPane passageScrollPane = new ScrollPane();
+        //TODO: setup Passage Content and make it scrollable if it's too large to fit. At the moment you can still scroll horizontally.
+        passageContent = new Text(game.getStory().getOpeningPassage().getContent() + "ejsdhfkjadshfkjdsahfksdfkjahsdkjhaskjhaksljhkjsdahvkljashvjasdhvjashvjashfjashfkjasfhjashfjkSHDFJKSHFJSHFJSDHFJHSDFKJHASDFHASDF@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@222");
+        passageContent.setFont(new Font(14));
+        passageContent.wrappingWidthProperty().bind(scene.widthProperty());
+        passageScrollPane.setMaxSize(800, 500);
+        passageScrollPane.setContent(passageContent);
+        VBox center = new VBox(storyTitle, passageTitle, passageScrollPane);
+        center.setMargin(passageContent, new Insets(25, 0, 0, 0));
+        center.setAlignment(Pos.TOP_CENTER);
+        root.getChildren().add(center);
+    }
 
+    public Scene getScene() {
+        configurePassageContent();
+        return scene;
     }
 }
