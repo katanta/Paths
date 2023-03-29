57aa033968a79b0da7f1b5751fbe1ef330c9e07a
Kris.JM <krisjm@stud.ntnu.no>
2023-03-29 14:40:31 +0200

    Make user able to navigate through a story with PassageView
    The user uses buttons tied to links to update the scene, going through the story

--- src/main/java/edu/ntnu/mappe32/view/PassageView.java
+++ src/main/java/edu/ntnu/mappe32/view/PassageView.java
@@ -1,11 +1,14 @@
 package edu.ntnu.mappe32.view;
 
 import edu.ntnu.mappe32.controller.Game;
+import edu.ntnu.mappe32.model.story_related.Passage;
+import javafx.beans.property.ReadOnlyDoubleProperty;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
+import javafx.scene.control.*;
+import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
@@ -18,35 +21,71 @@ public class PassageView {
     Label storyTitle;
     Text passageTitle;
     Game game;
+    Passage currentPassage;
+    VBox linkButtons;
 
     public PassageView(Game game) {
         this.game = game;
     }
 
     public void configurePassageContent() {
+        currentPassage = game.begin();
         StackPane root = new StackPane();
         scene = new Scene(root, 1280, 720);
         storyTitle = new Label(game.getStory().getTitle());
         storyTitle.setLabelFor(passageTitle);
         storyTitle.setMinSize(10, 10);
         storyTitle.setFont(new Font(45));
-        passageTitle = new Text(game.getStory().getOpeningPassage().getTitle());
+        passageTitle = new Text(currentPassage.getTitle());
         passageTitle.setFont(new Font(25));
         ScrollPane passageScrollPane = new ScrollPane();
-        //TODO: setup Passage Content and make it scrollable if it's too large to fit. At the moment you can still scroll horizontally.
-        passageContent = new Text(game.getStory().getOpeningPassage().getContent() + "ejsdhfkjadshfkjdsahfksdfkjahsdkjhaskjhaksljhkjsdahvkljashvjasdhvjashvjashfjashfkjasfhjashfjkSHDFJKSHFJSHFJSDHFJHSDFKJHASDFHASDF@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@222");
+        passageContent = new Text(currentPassage.getContent() + " ejsdhfkjadshfkjdsahfksdfkjahsdkjhaskjhaksljhkjsdahvkljashvjasdhvjashvjashfjashfkjasfhjashfjkSHDFJKSHFJSHFJSDHFJHSDFKJHASDFHASDF@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@222");
         passageContent.setFont(new Font(14));
-        passageContent.wrappingWidthProperty().bind(scene.widthProperty());
-        passageScrollPane.setMaxSize(800, 500);
+        passageScrollPane.setMaxWidth(600);
+        passageContent.setWrappingWidth(passageScrollPane.getMaxWidth() - 15);
+        passageScrollPane.setMaxHeight(100);
         passageScrollPane.setContent(passageContent);
         VBox center = new VBox(storyTitle, passageTitle, passageScrollPane);
         center.setMargin(passageContent, new Insets(25, 0, 0, 0));
         center.setAlignment(Pos.TOP_CENTER);
         root.getChildren().add(center);
+
+        //Configure initial links
+        ScrollPane linksScrollPane = new ScrollPane();
+        linkButtons = new VBox();
+        game.begin().getLinks().forEach(link -> {
+            Button linkButton = new Button(link.getText());
+            linkButton.setPrefSize(200, 40);
+            linkButton.setOnMouseClicked(mouseEvent -> {currentPassage = game.go(link); //this.game can be used to update player information
+            updateScene(); //make the buttons change the current active passage, changing the scene
+            });
+            linkButton.setTooltip(new Tooltip(link.getText()));
+            linkButtons.getChildren().add(linkButton);
+        });
+        linkButtons.setAlignment(Pos.CENTER);
+        linksScrollPane.setContent(linkButtons);
+        linksScrollPane.setMaxSize(200, 500);
+        linksScrollPane.setFitToWidth(true);
+        center.getChildren().add(linksScrollPane);
+        center.setMargin(linksScrollPane, new Insets(25, 0, 0, 0));
     }
 
     public Scene getScene() {
         configurePassageContent();
         return scene;
+    }
+
+    public void updateScene() {
+        linkButtons.getChildren().clear();
+        passageContent.setText(currentPassage.getContent());
+        passageTitle.setText(currentPassage.getTitle());
+        currentPassage.getLinks().forEach(link -> {
+            Button linkButton = new Button(link.getText());
+            linkButton.setPrefSize(200, 40);
+            linkButton.setOnMouseClicked(mouseEvent -> {this.currentPassage = game.go(link);
+                updateScene();
+            });
+            linkButtons.getChildren().add(linkButton);
+        });
     }
 }
