d2f54e2b07987d1bb4b32eb66878a1903ac049c4
Yasin Ali Marouga <yasinam@stud.ntnu.no>
2023-05-15 10:40:23 +0200

    Add new goalsListView and addGoalButton to CreatePlayerAndGoalsView

--- src/main/java/edu/ntnu/mappe32/view/CreatePlayerAndGoalsView.java
+++ src/main/java/edu/ntnu/mappe32/view/CreatePlayerAndGoalsView.java
@@ -1,6 +1,7 @@
 package edu.ntnu.mappe32.view;
 
 import edu.ntnu.mappe32.model.PathsFile;
+import edu.ntnu.mappe32.model.goal_related.Goal;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
@@ -34,6 +35,8 @@ public class CreatePlayerAndGoalsView {
     TextField goalValueTextField;
     Label numberValueGoalLabel;
     ImageView playButton;
+    ListView<Goal> goalsListView;
+    Button addGoalButton;
 
 
     public CreatePlayerAndGoalsView(PathsFile pathsFile) {
@@ -53,49 +56,58 @@ public class CreatePlayerAndGoalsView {
         configureGoalValueTextField();
         configureNumberValueGoalLabel();
         configurePlayButton();
+        configureGoalsListView();
+        configureAddGoalButton();
 
         HBox topBar = new HBox(10);
         topBar.getChildren().addAll(storyTitleLabel, pathsFileLabel);
         HBox goalTypeToggleBar = new HBox();
-        goalTypeToggleBar.getChildren().addAll(goldToggle, healthToggle, inventoryToggle, scoreToggle   );
-        HBox goalValueBar = new HBox(10);
-        goalValueBar.getChildren().addAll(numberValueGoalLabel, goalValueTextField);
+        goalTypeToggleBar.getChildren().addAll(goldToggle, healthToggle, inventoryToggle, scoreToggle);
+        HBox goalValueBar = new HBox();
+        goalValueBar.getChildren().addAll(numberValueGoalLabel, goalValueTextField, addGoalButton);
         VBox playerAndGoalConfigurationBox = new VBox(10);
         playerAndGoalConfigurationBox.getChildren().addAll(topBar,
                 playerConfigurationLabel, playerGridPane, addGoalLabel,
-                goalTypeToggleBar, goalValueBar, playButton);
+                goalTypeToggleBar, goalValueBar, playButton, goalsListView);
 
         StackPane root = new StackPane();
         root.getChildren().addAll(playerAndGoalConfigurationBox);
         this.scene = new Scene(root, 1280, 720);
 
     }
-    public void configureStoryTitleLabel() {
+    private void configureStoryTitleLabel() {
         this.storyTitleLabel = new Label(pathsFile.getStoryTitle());
         storyTitleLabel.setFont(new Font("Times New Roman", 40));
         HBox.setMargin(storyTitleLabel, new Insets(10));
 
     }
-    public void configurePlayerNameLabel() {
+    private void configureAddGoalButton() {
+        this.addGoalButton = new Button("Add Goal");
+        addGoalButton.setVisible(false);
+        addGoalButton.setManaged(false);
+        HBox.setMargin(addGoalButton, new Insets(10));
+
+    }
+    private void configurePlayerNameLabel() {
         this.playerNameLabel = new Label("Player Name:");
         playerNameLabel.setFont(new Font("Times New Roman", 20));
     }
-    public void configurePlayerHealthLabel() {
+    private void configurePlayerHealthLabel() {
         this.playerHealthLabel = new Label("Player Health:");
         playerHealthLabel.setFont(new Font("Times New Roman", 20));
     }
-    public void configurePlayerGoldLabel() {
+    private void configurePlayerGoldLabel() {
         this.playerGoldLabel = new Label("Player Gold:");
         playerGoldLabel.setFont(new Font("Times New Roman", 20));
     }
-    public void configureAddGoalLabel() {
+    private void configureAddGoalLabel() {
         this.addGoalLabel = new Label("Add Goals:");
         addGoalLabel.setFont(new Font("Times New Roman", 20));
         VBox.setMargin(addGoalLabel, new Insets(10));
     }
 
 
-    public void configureNumberValueGoalLabel() {
+    private void configureNumberValueGoalLabel() {
         this.numberValueGoalLabel = new Label();
         numberValueGoalLabel.setFont(new Font("Times New Roman", 20));
         numberValueGoalLabel.setManaged(false);
@@ -103,7 +115,7 @@ public class CreatePlayerAndGoalsView {
         HBox.setMargin(numberValueGoalLabel, new Insets(10));
 
     }
-    public void configureGoalTypeToggleButton() {
+    private void configureGoalTypeToggleButton() {
 
         this.goalTypeToggle = new ToggleGroup();
         goldToggle = new ToggleButton("Gold");
@@ -124,7 +136,7 @@ public class CreatePlayerAndGoalsView {
         HBox.setMargin(scoreToggle, new Insets(5));
     }
 
-    public void configurePlayerGridPane() {
+    private void configurePlayerGridPane() {
         playerGridPane = new GridPane();
         playerGridPane.setHgap(10);
         playerGridPane.setVgap(10);
@@ -139,12 +151,12 @@ public class CreatePlayerAndGoalsView {
         GridPane.setHgrow(playerHealthTextField, Priority.ALWAYS);
         VBox.setMargin(playerGridPane, new Insets(10));
     }
-    public void configurePlayerNameTextField() {
+    private void configurePlayerNameTextField() {
         this.playerNameTextField = new TextField();
         playerNameTextField.setPromptText("Player Name");
         playerNameTextField.setMaxWidth(200);
     }
-    public void configureGoalValueTextField() {
+    private void configureGoalValueTextField() {
         this.goalValueTextField = new TextField();
         goalValueTextField.setPromptText("Enter the value of the goal");
         goalValueTextField.setMaxWidth(200);
@@ -152,22 +164,22 @@ public class CreatePlayerAndGoalsView {
         goalValueTextField.setManaged(false);
         HBox.setMargin(goalValueTextField, new Insets(10));
     }
-    public void configurePlayerGoldTextField() {
+    private void configurePlayerGoldTextField() {
         this.playerGoldTextField = new TextField();
         playerGoldTextField.setPromptText("Player Gold");
         playerGoldTextField.setMaxWidth(200);
     }
-    public void configurePlayerHealthTextField() {
+    private void configurePlayerHealthTextField() {
         this.playerHealthTextField = new TextField();
         playerHealthTextField.setPromptText("Player Health");
         playerHealthTextField.setMaxWidth(200);
     }
-    public void configurePlayerConfigurationLabel() {
+    private void configurePlayerConfigurationLabel() {
         this.playerConfigurationLabel = new Label("Player Configuration");
         playerConfigurationLabel.setFont(new Font("Times New Roman", 20));
         VBox.setMargin(playerConfigurationLabel, new Insets(10));
     }
-    public void configurePathsFileLabelInfo() {
+    private void configurePathsFileLabelInfo() {
         this.pathsFileLabel = new Label("Broken Links: "
                 + pathsFile.getBrokenLinks() + "\nFile Path: "
                 + pathsFile.getFilePath());
@@ -175,14 +187,32 @@ public class CreatePlayerAndGoalsView {
         this.pathsFileLabel.setWrapText(true);
         HBox.setMargin(pathsFileLabel, new Insets(10));
     }
-    public void configurePlayButton() {
-
+    private void configurePlayButton() {
         try {
             playButton = new ImageView(new Image(new FileInputStream("src/main/resources/img/play.png")));
         } catch (FileNotFoundException e) {
             throw new RuntimeException("play.png could not be fount in src/main/resources/img");
         }
         playButton.setPickOnBounds(true);
+    }
+    private void configureGoalsListView() {
+        this.goalsListView = new ListView<>();
+        goalsListView.setPrefWidth(100);
+        goalsListView.setPrefHeight(300);
+
+        goalsListView.setCellFactory(param -> new ListCell<>() {
+            @Override
+            protected void updateItem(Goal goal, boolean empty) {
+                super.updateItem(goal, empty);
+                if (goal == null || empty) {
+                    setText("");
+                    setStyle("");
+                } else {
+                    GoalCell cell = new GoalCell(goal);
+                    setGraphic(cell.getHBox());
+                    }
+                }
+        });
     }
     public Label getNumberValueGoalLabel() {
         return numberValueGoalLabel;
@@ -229,5 +259,13 @@ public class CreatePlayerAndGoalsView {
 
     public TextField getPlayerNameTextField() {
         return playerNameTextField;
+    }
+
+    public Button getAddGoalButton() {
+        return addGoalButton;
+    }
+
+    public ListView<Goal> getGoalsListView() {
+        return goalsListView;
     }
 }
