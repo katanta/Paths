428e2847dac3caeeed769532aec1f5cf692f9187
Kristians J. Matrevics <krisjm@stud.ntnu.no>
2023-05-19 15:01:31 +0200

    Complete unconnected paths SplashScreenView, background to be added

--- src/main/java/edu/ntnu/mappe32/controller/FinalSplashScreenController.java
+++ src/main/java/edu/ntnu/mappe32/controller/FinalSplashScreenController.java
@@ -1,4 +1,51 @@
 package edu.ntnu.mappe32.controller;
 
+import edu.ntnu.mappe32.view.FinalSplashScreenView;
+import javafx.geometry.Pos;
+import javafx.scene.Cursor;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class FinalSplashScreenController {
+    FinalSplashScreenView splashScreenView;
+    Stage stage;
+
+    public FinalSplashScreenController(Stage stage, FinalSplashScreenView splashScreenView) {
+        this.stage = stage;
+        this.splashScreenView = splashScreenView;
+        configureButtonActions();
+    }
+
+    private void configureButtonActions() {
+        splashScreenView.getPlayButton().setOnMouseEntered(e -> {
+            splashScreenView.getPlayButtonHBox().getChildren().get(0).setVisible(true);
+            splashScreenView.getPlayButtonHBox().getChildren().get(2).setVisible(true);
+            splashScreenView.getPlayButton().setStyle(("-fx-border-color: #ffffff; -fx-border-width: 5px; -fx-background-color: #000000; -fx-text-fill: #ffffff;"));
+        });
+        splashScreenView.getPlayButton().setOnMouseExited(e -> {
+            splashScreenView.getPlayButton().setStyle("-fx-border-color: #000000; -fx-border-width: 5px; -fx-background-color: #ffffff; -fx-text-fill: #000000");
+            splashScreenView.getPlayButtonHBox().getChildren().get(0).setVisible(false);
+            splashScreenView.getPlayButtonHBox().getChildren().get(2).setVisible(false);
+        });
+        splashScreenView.getPlayButton().setOnMouseClicked(e -> {
+            splashScreenView.getPlayButton().setStyle("-fx-background-color: #888383");
+        });
+        splashScreenView.getPlayButton().setOnMouseReleased(e -> {
+            //Todo: change to show existing stories
+        });
+
+        splashScreenView.getBaitButtonHBox().getChildren().get(1).setOnMouseEntered(e -> {
+            splashScreenView.getBaitButtonHBox().getChildren().get(0).setVisible(true);
+            splashScreenView.getBaitButtonHBox().getChildren().get(2).setVisible(true);
+        });
+        splashScreenView.getBaitButtonHBox().getChildren().get(1).setOnMouseExited(e -> {
+            splashScreenView.getBaitButtonHBox().getChildren().get(0).setVisible(false);
+            splashScreenView.getBaitButtonHBox().getChildren().get(2).setVisible(false);
+        });
+        splashScreenView.getBaitButtonHBox().getChildren().get(1).setOnMouseClicked(e -> {
+            //Todo: add a positively hilarious sound effect to hoodwink, bamboozle, lead astray, run amok and flat out deceive the players.
+        });
+    }
+
+
 }
--- src/main/java/edu/ntnu/mappe32/view/FinalSplashScreenView.java
+++ src/main/java/edu/ntnu/mappe32/view/FinalSplashScreenView.java
@@ -4,6 +4,7 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Tooltip;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
@@ -19,13 +20,16 @@ public class FinalSplashScreenView {
     private final Scene scene;
     private StackPane root;
     private Image bigPathsLogo;
-    private VBox menuButtonVBox;
+    private VBox buttonsVBoxContainer;
+    private HBox playButtonHBox;
+    private Button playButton;
+    private HBox baitButtonHBox;
 
     public FinalSplashScreenView() {
         root = new StackPane();
         root.setAlignment(Pos.TOP_CENTER);
         configureLogo(); //configureLogo must be done first to apply correct margins for it
-        configureMenuButtonVBox();
+        configureButtonsVBox();
         scene = new Scene(root, 1280, 720);
     }
 
@@ -39,21 +43,76 @@ public class FinalSplashScreenView {
         StackPane.setMargin(root.getChildren().get(0), new Insets(30, 0, 0, 0));
     }
 
-    private void configureMenuButtonVBox() {
-        Button playButton = new Button("Play an existing story");
+
+    private void configureButtonsVBox(){
+        buttonsVBoxContainer = new VBox();
+        buttonsVBoxContainer.setAlignment(Pos.CENTER);
+        buttonsVBoxContainer.setSpacing(20);
+        configurePlayButtonHBox();
+        configureBaitButtonHBox();
+        root.getChildren().add(buttonsVBoxContainer);
+    }
+    private void configurePlayButtonHBox() {
+        playButton = new Button("Play an existing story");
         playButton.setFont(resizableMainFont(30));
         playButton.setStyle("-fx-border-color: #000000; -fx-border-width: 5px; -fx-background-color: #ffffff");
-        //Button baitButton = new Button("Create your own path");
+        playButtonHBox = new HBox(playButton);
+        configureSelectionPointers(playButtonHBox);
+        playButtonHBox.getChildren().get(0).setVisible(false);
+        playButtonHBox.getChildren().get(2).setVisible(false);
+        playButtonHBox.setAlignment(Pos.CENTER);
+        playButtonHBox.setSpacing(50);
+        StackPane.setMargin(playButtonHBox, new Insets(20, 30, 0, 0));
+        buttonsVBoxContainer.getChildren().add(playButtonHBox);
+    }
 
-        menuButtonVBox = new VBox(playButton);
-        menuButtonVBox.setAlignment(Pos.CENTER);
-        StackPane.setMargin(menuButtonVBox, new Insets(20, 0, 0, 0));
-        root.getChildren().add(menuButtonVBox);
+    private void configureSelectionPointers(HBox hBox) {
+        ImageView pointer;
+        ImageView pointerClone;
+        try {
+            pointer = new ImageView(new Image(new FileInputStream("src/main/resources/img/Pointer.png")));
+            pointer.setFitWidth(75);
+            pointer.setFitHeight(75);
+            pointer.setRotate(270);
+            pointerClone = new ImageView(pointer.getImage());
+            pointerClone.setRotate(90);
+            pointerClone.setFitWidth(pointer.getFitWidth());
+            pointerClone.setFitHeight(pointer.getFitHeight());
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        hBox.getChildren().add(0, pointer);
+        hBox.getChildren().add(2, pointerClone);
+    }
+
+    private void configureBaitButtonHBox() {
+        Button baitButton = new Button("Follow your own path \n     (COMING SOON!)");
+        baitButton.setFont(resizableMainFont(30));
+        baitButton.setStyle("-fx-border-color: #000000; -fx-border-width: 5px; -fx-background-color: #6b6b6b");
+        baitButton.setTooltip(new Tooltip("This Feature is unavailable but you can still click me :)"));
+        baitButton.getTooltip().setFont(resizableMainFont(15));
+        baitButtonHBox = new HBox(baitButton);
+        baitButtonHBox.setSpacing(50);
+        configureSelectionPointers(baitButtonHBox);
+        baitButtonHBox.getChildren().get(0).setVisible(false);
+        baitButtonHBox.getChildren().get(2).setVisible(false);
+        baitButtonHBox.setAlignment(Pos.CENTER);
+        buttonsVBoxContainer.getChildren().add(baitButtonHBox);
     }
 
     public Scene getScene() {
         return scene;
     }
 
+    public HBox getPlayButtonHBox() {
+        return playButtonHBox;
+    }
 
+    public Button getPlayButton() {
+        return playButton;
+    }
+
+    public HBox getBaitButtonHBox() {
+        return baitButtonHBox;
+    }
 }
