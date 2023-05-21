8e6a23630db55487b1a2d0f60ef5c9fbc24456d7
Yasin Ali Marouga <yasinam@stud.ntnu.no>
2023-05-21 20:04:48 +0200

    Remove unused exception classes

--- src/main/java/edu/ntnu/mappe32/model/action_related/InsufficientGoldException.java
+++
@@ -1,8 +0,0 @@
-package edu.ntnu.mappe32.model.action_related;
-
-public class InsufficientGoldException extends Exception {
-    public InsufficientGoldException(String message) {
-        super(message);
-    }
-}
-
--- src/main/java/edu/ntnu/mappe32/model/action_related/PlayerDoesNotHaveItemException.java
+++
@@ -1,7 +0,0 @@
-package edu.ntnu.mappe32.model.action_related;
-
-public class PlayerDoesNotHaveItemException extends Exception{
-    public PlayerDoesNotHaveItemException(String message) {
-        super(message);
-    }
-}
